0355c67439fe28a7a2da1f0feb049272f8cbbb38
David Vilchez <dvilchez@xuaps.com>
2014-05-28 23:26:03 +0200

    locales file updated

--- builds/web/lib/plugins/locales.js
+++ builds/web/lib/plugins/locales.js
@@ -1,139 +1,153 @@
-ig.module( 
-	'plugins.locales' 
+ig.module(
+'plugins.locales'
 )
 .requires(
-	
 )
 .defines(function(){
-	ig.locales = { 
-		'es':{ 
-			translation:{
-				"rojo": {
-					"presentacion":"¡Hola! Yo soy el color Rojo.",
-					"frase":"Puedes encontrarme en comida tan rica...\n¡como las fresas! ¡Mmm!"
-				},
-
-				"naranja": {
-					"presentacion":"Pues yo me llamo Naranja.",
-					"frase":"Soy tan famoso\nque hasta una fruta lleva mi nombre."
-				},
-
-				"amarillo": {
-					"presentacion":"¡Aquí estoy!\nSoy el color Amarillo.",
-					"frase":"Fíjate cómo salgo\nde los rayos del sol."
-				},
-
-				"verde": {
-					"presentacion":"Yo soy el color Verde.",
-					"frase":"Estoy en la hierba y en las hojas.\n¡La naturaleza es lo mío!"
-				},
-
-				"azul": {
-					"presentacion":"A mí todos me llaman Azul.",
-					"frase": "¡Y ocupo toooodo el cielo!"
-				},
-
-				"anil": {
-					"presentacion":"Me presentaré,\nsoy el color Añil.",
-					"frase": "¡Puedes encontrarme bailando\ncon los peces entre las olas del mar!"
-				},
-
-				"preferencias": {
-					"titulo": "Preferencias",
-					"idioma": "Idioma",
-					"sonido": "Sonido",
-					"narracion": "Narración",
-					"musica": "Música",
-					"aceptar": "Aceptar",
-					"cancelar": "Cancelar"
-				}
-			},
-		},
-		'gl':{ 
-			translation:{
-				"rojo": {
-					"presentacion":"¡Hola! Yo soy el color Rojo.",
-					"frase":"Puedes encontrarme en comida tan rica...\n¡como las fresas! ¡Mmm!"
-				},
-
-				"naranja": {
-					"presentacion":"Pues yo me llamo Naranja.",
-					"frase":"Soy tan famoso\nque hasta una fruta lleva mi nombre."
-				},
-
-				"amarillo": {
-					"presentacion":"¡Aquí estoy!\nSoy el color Amarillo.",
-					"frase":"Fíjate cómo salgo\nde los rayos del sol."
-				},
-
-				"verde": {
-					"presentacion":"Yo soy el color Verde.",
-					"frase":"Estoy en la hierba y en las hojas.\n¡La naturaleza es lo mío!"
-				},
-
-				"azul": {
-					"presentacion":"A mí todos me llaman Azul.",
-					"frase": "¡Y ocupo toooodo el cielo!"
-				},
-
-				"anil": {
-					"presentacion":"Me presentaré,\nsoy el color Añil.",
-					"frase": "¡Puedes encontrarme bailando\ncon los peces entre las olas del mar!"
-				},
-
-				"preferencias": {
-					"titulo": "Ni idea de como se dice en gallego",
-					"idioma": "Idioma",
-					"sonido": "Sonido",
-					"narracion": "Narración",
-					"musica": "Música",
-					"aceptar": "Aceptar",
-					"cancelar": "Cancelar"
-				}
-			},
-		},
-		'en':{ 
-			translation:{
-				"rojo": {
-					"presentacion":"¡Hola! Yo soy el color Rojo.",
-					"frase":"Puedes encontrarme en comida tan rica...\n¡como las fresas! ¡Mmm!"
-				},
-
-				"naranja": {
-					"presentacion":"Pues yo me llamo Naranja.",
-					"frase":"Soy tan famoso\nque hasta una fruta lleva mi nombre."
-				},
-
-				"amarillo": {
-					"presentacion":"¡Aquí estoy!\nSoy el color Amarillo.",
-					"frase":"Fíjate cómo salgo\nde los rayos del sol."
-				},
-
-				"verde": {
-					"presentacion":"Yo soy el color Verde.",
-					"frase":"Estoy en la hierba y en las hojas.\n¡La naturaleza es lo mío!"
-				},
-
-				"azul": {
-					"presentacion":"A mí todos me llaman Azul.",
-					"frase": "¡Y ocupo toooodo el cielo!"
-				},
-
-				"anil": {
-					"presentacion":"Me presentaré,\nsoy el color Añil.",
-					"frase": "¡Puedes encontrarme bailando\ncon los peces entre las olas del mar!"
-				},
-
-				"preferencias": {
-					"titulo": "Settings",
-					"idioma": "Idioma",
-					"sonido": "Sonido",
-					"narracion": "Narración",
-					"musica": "Música",
-					"aceptar": "Aceptar",
-					"cancelar": "Cancelar"
-				}
-			}
-		},
-	};
+ig.locales ={
+'es':{
+translation:{
+"rojo":{
+"presentacion":"¡Hola! Yo soy el color Rojo.",
+"frase":"Puedes encontrarme en comida tan rica...\n¡como las fresas! ¡Mmm!"
+},
+"naranja":{
+"presentacion":"Pues yo me llamo Naranja.",
+"frase":"Soy tan famoso\nque hasta una fruta lleva mi nombre."
+},
+"amarillo":{
+"presentacion":"¡Aquí estoy!\nSoy el color Amarillo.",
+"frase":"Fíjate cómo salgo\nde los rayos del sol."
+},
+"verde":{
+"presentacion":"Yo soy el color Verde.",
+"frase":"Estoy en la hierba y en las hojas.\n¡La naturaleza es lo mío!"
+},
+"azul":{
+"presentacion":"A mí todos me llaman Azul.",
+"frase":"¡Y ocupo toooodo el cielo!"
+},
+"anil":{
+"presentacion":"Me presentaré:\nSoy el color Añil.",
+"frase":"¡Puedes encontrarme bailando\ncon los peces entre las olas del mar!"
+},
+"violeta":{
+"presentacion":"A mí me conocen como Violeta.",
+"frase":"Y soy tan delicada que he cedido\nmi nombre a estas bonitas flores."
+},
+"todos":{
+"amigos":"¡Somos grandes amigos y por eso\nnos puedes encontrar mezclados y en grupos!",
+"pregunta":"Pero...\n¿Sabes dónde puedes encontrarnos a todos juntos?"
+},
+"respuesta":{
+"correcto":"¡Eso es!\n¡En el Arco Iris!"
+},
+"preferencias":{
+"titulo":"Configuración",
+"idioma":"Idioma",
+"sonido":"Sonido",
+"narracion":"Narración",
+"musica":"Música",
+"aceptar":"Aceptar",
+"cancelar":"Cancelar"
+}
+},
+},
+'gl':{
+translation:{
+"rojo":{
+"presentacion":"Ola! Eu son a cor Vermella.",
+"frase":"Podes atoparme en comida tan rica...\ncomo os amorodos! Mmm!"
+},
+"naranja":{
+"presentacion":"Pois eu chámome Laranxa.",
+"frase":"Sonche tan famoso\nque ata unha froita leva o meu nome."
+},
+"amarillo":{
+"presentacion":"Velaquí estou!\nSon a cor Amarela.",
+"frase":"Mira como saio\ndos raios do sol."
+},
+"verde":{
+"presentacion":"Eu son a cor Verde.",
+"frase":"Estou na herba e nas follas.\nA natureza é o meu!"
+},
+"azul":{
+"presentacion":"A min chámanme todos Azul.",
+"frase":"E ocupo toooodo o ceo!"
+},
+"anil":{
+"presentacion":"Voume presentar:\nSon a cor Anil.",
+"frase":"Pódesme atopar bailando cos peixes\nentre as ondas do mar!"
+},
+"violeta":{
+"presentacion":"A min coñécenme como Violeta.",
+"frase":"E son tan delicada que cedín o meu nome a estas bonitas flores."
+},
+"todos":{
+"amigos":"Somos moi amigos e por iso\npódesnos atopar mesturados e en grupos!",
+"pregunta":"Pero...\nSabes onde nos podes atopar a todos xuntos?"
+},
+"respuesta":{
+"correcto":"Iso é!\nNo Arco da Vella!"
+},
+"preferencias":{
+"titulo":"Configuración",
+"idioma":"Idioma",
+"sonido":"Sonido",
+"narracion":"Narración",
+"musica":"Música",
+"aceptar":"Aceptar",
+"cancelar":"Cancelar"
+}
+},
+},
+'en':{
+translation:{
+"rojo":{
+"presentacion":"Hello! I am the colour Red.",
+"frase":"You can find me in food such as...\ndelicious strawberries! Om nom nom!!"
+},
+"naranja":{
+"presentacion":"Well, my name is Orange.",
+"frase":"I am so famous\nthat even a fruit has my name."
+},
+"amarillo":{
+"presentacion":"Here I am!\nThe colour Yellow!",
+"frase":"Look how I go out of the rays of the sun."
+},
+"verde":{
+"presentacion":"I am the colour Green.",
+"frase":"I am in the grass and the leaves.\nNature is my thing!"
+},
+"azul":{
+"presentacion":"Everybody calls me Blue.",
+"frase":"And I cover the whoooole sky!"
+},
+"anil":{
+"presentacion":"Allow me to introduce myself:\nMy name is Indigo.",
+"frase":"You can find me dancing with the fish\nbetween the waves of the sea!"
+},
+"violeta":{
+"presentacion":"People knows me as Violet.",
+"frase":"And I am so delicate that I\nlend my name to these beautiful flowers."
+},
+"todos":{
+"amigos":"We are really good friends and that is why\nyou can find us mixed or in groups!",
+"pregunta":"But...\nDo you know where you can find us all together?"
+},
+"respuesta":{
+"correcto":"That's right!\nIn the Rainbow!"
+},
+"preferencias":{
+"titulo":"Settings",
+"idioma":"Language",
+"sonido":"Sound",
+"narracion":"Narration",
+"musica":"Music",
+"aceptar":"Confirm",
+"cancelar":"Cancel"
+}
+}
+},
+};
 });
